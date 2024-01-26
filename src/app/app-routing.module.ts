e1ff2f8c3653c31ea2c669e05c1f016009aa890a
Greg <greg_sr@hotmail.com>
2024-01-26 13:07:40 +0100

    structure de base des composants (html)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ListeProduitsComponent } from './produits/liste-produits/liste-produits.component';
+import { PanierComponent } from './achats/panier/panier.component';
+import { ProfilComponent } from './profil/profil.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'connexion', component: ConnexionComponent},
+  {path: 'inscription', component: InscriptionComponent},
+  {path: 'liste', component: ListeProduitsComponent},
+  {path: 'panier', component: PanierComponent},
+  {path: 'profil', component: ProfilComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,23 @@ import { HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
+import { ConnexionComponent } from './connexion/connexion.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ListeProduitsComponent } from './produits/liste-produits/liste-produits.component';
+import { PanierComponent } from './achats/panier/panier.component';
+import { ProfilComponent } from './profil/profil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    ConnexionComponent,
+    InscriptionComponent,
+    ListeProduitsComponent,
+    PanierComponent,
+    ProfilComponent
   ],
   imports: [
     BrowserModule,
