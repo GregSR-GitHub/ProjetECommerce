8cc07bc894d49bc230b325f4e9a78a6250ef98aa
Greg <greg_sr@hotmail.com>
2024-01-29 16:50:53 +0100

    ajout api pour inscription et profil.

--- src/app/inscription/inscription.component.ts
+++ src/app/inscription/inscription.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
+import { Client } from '../modeles/client';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-inscription',
   templateUrl: './inscription.component.html',
   styleUrls: ['./inscription.component.css']
 })
+
 export class InscriptionComponent {
+  status:string = 'New';
+  client: Client = new Client();
+  login:string;
+  pass:string;
+  nom:string;
+  prenom:string;
+  adresse:string;
+  errLogin:string;
+
+  constructor(private http:HttpClient){}
+
+  ngOnInit(): void {
+
+  }
+
+  newClient():void{
+
+    this.client.login = this.login;
+    this.client.pass = this.pass;
+    this.client.nom = this.nom;
+    this.client.prenom = this.prenom;
+    this.client.adresse = this.adresse;
+
+    const body = JSON.stringify(this.client);
+    this.http.post("http://localhost:57070/api/Client/",body,{
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }).subscribe(response => {
+      console.log("Ok");
+      this.status = "Ok";
+    },
+
+      err => {
+        this.errLogin="Le formulaire est incomplet ou ce nom d'utilisateur existe déja."
+      });
+  }
 
 }
+
--- src/app/profil/profil.component.ts
+++ src/app/profil/profil.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Commande } from '../modeles/commande';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { Client } from '../modeles/client';
 
 @Component({
   selector: 'app-profil',
@@ -6,5 +10,76 @@ import { Component } from '@angular/core';
   styleUrls: ['./profil.component.css']
 })
 export class ProfilComponent {
+  listeCommande: Array<Commande>;
+  client:Client;
+  adresse:string;
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
+
+  ngOnInit():void{
+
+    let str2: string = sessionStorage.getItem("client");
+    let c:Client = JSON.parse(str2);
+
+    this.http.get<Client>("http://localhost:57070/api/Client/"+c.id).subscribe(
+      (response) => {
+        this.client=response;
+        this.adresse = this.client.adresse;
+      }
+      ,
+     (err) => {
+        console.log("KO")
+        
+      },
+
+      () => {
+        console.log("*********complete****")
+        
+      }
+    );
+
+    this.http.get<Array<Commande>>("http://localhost:57070/api/Commande").subscribe(
+      (response) => {
+        this.listeCommande=response;
+       
+        console.log(response);
+      }
+      ,
+     (err) => {
+        console.log("KO")
+        
+      },
+
+      () => {
+        console.log("*********complete****")
+        
+      }
+    );
+    
+  }
+
+  update()
+  {
+    this.client.adresse = this.adresse;
+    const body = JSON.stringify(this.client);
+    
+    this.http.put("http://localhost:57070/api/Client", body, {
+      headers: new HttpHeaders({
+        "Content-Type": "application/json"
+      })
+    }).
+      subscribe(response => {
+
+     
+        console.log("update ok");
+
+      },
+
+        err => {
+        
+          console.log("update KO");
+
+        });
+  }
 
 }
